17c57551ca8cef9c2a94aab1964d1775b4b8df00
Kentangz <adhitsholawat12@gmail.com>
2025-01-04 00:00:42 +0700

    optimize admin recouces

--- backend-laravel/app/Http/Resources/AdminResource.php
+++ backend-laravel/app/Http/Resources/AdminResource.php
@@ -19,37 +19,35 @@ class AdminResource extends JsonResource
 
     public function toArray(Request $request): array
     {
-        // Cek apakah resource adalah koleksi atau model tunggal
+ 
         if ($this->resource instanceof \Illuminate\Database\Eloquent\Collection) {
-            // Jika resource adalah koleksi (banyak data)
             return [
                 'success' => $this->status,
                 'message' => $this->message,
                 'data' => $this->resource->map(function ($admin) {
                     return [
-                        'id' => $admin->id, // Mengakses id pada setiap admin di koleksi
+                        'id' => $admin->id, 
                         'nama' => $admin->nama,
                         'email' => $admin->email,
                         'password' => $admin->password,
                         'created_at' => $admin->created_at,
                         'updated_at' => $admin->updated_at,
-                        'image_url' => $admin->image,  // Menggunakan URL gambar yang benar
+                        'image_url' => $admin->image,  
                     ];
                 }),
             ];
         } else {
-            // Jika resource adalah model tunggal (1 data admin)
             return [
                 'success' => $this->status,
                 'message' => $this->message,
                 'data' => [
-                    'id' => $this->resource->id, // Mengakses id pada model tunggal
+                    'id' => $this->resource->id, 
                     'nama' => $this->resource->nama,
                     'email' => $this->resource->email,
                     'password' => $this->resource->password,
                     'created_at' => $this->resource->created_at,
                     'updated_at' => $this->resource->updated_at,
-                    'image_url' => $this->resource->image,  // Menggunakan URL gambar yang benar
+                    'image_url' => $this->resource->image,
                 ],
             ];
         }
